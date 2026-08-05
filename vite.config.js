e2fd8fe2ffed06eb73f789dfd7ba19d71d9afdad
LuxSiteCZ <krulkostudio@gmail.com>
2026-08-05 17:05:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,40 @@
-import { defineConfig } from "vite";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
-
-export default defineConfig({
-  plugins: [injectHTML(), FullReload(["./partials/*.html"])],
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/vite-webstudio/' : '/',
+  plugins: [injectHTML(), FullReload(['./partials/*.html'])],
 
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     emptyOutDir: true,
 
     rollupOptions: {
       input: {
-        main: "./index.html",
+        main: './index.html',
       },
 
       output: {
-        entryFileNames: "js/main.js",
-        chunkFileNames: "js/[name].js",
-        assetFileNames: (assetInfo) => {
-          const name = assetInfo.name || "";
+        entryFileNames: 'js/main.js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: assetInfo => {
+          const name = assetInfo.name || '';
 
-          if (name.endsWith(".css")) {
-            return "css/main.css";
+          if (name.endsWith('.css')) {
+            return 'css/main.css';
           }
 
           if (/\.(png|jpg|jpeg|svg|gif|webp)$/i.test(name)) {
-            return "img/[name][extname]";
+            return 'img/[name][extname]';
           }
 
           if (/\.(woff|woff2|ttf|otf)$/i.test(name)) {
-            return "fonts/[name][extname]";
+            return 'fonts/[name][extname]';
           }
 
-          return "assets/[name][extname]";
+          return 'assets/[name][extname]';
         },
       },
     },
   },
-});
+}));
